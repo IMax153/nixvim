e9a97a1591e2af74087e03a2b4165a68c403ace8
Maxwell Brown <maxwellbrown1990@gmail.com>
2026-04-15 15:15:29 -0400

    enhance rust analyzer

--- config/plugins/languages/rust.nix
+++ config/plugins/languages/rust.nix
@@ -2,6 +2,20 @@
   plugins.lsp.servers = {
     rust_analyzer = {
       enable = true;
+      packageFallback = true;
+      cmd = [ "rust-analyzer" ];
+      settings = {
+        cargo = {
+          target = "x86_64-unknown-linux-gnu";
+          targetDir = true;
+        };
+        check = {
+          command = "check";
+          allTargets = false;
+          targets = [ "x86_64-unknown-linux-gnu" ];
+        };
+      };
+
       installCargo = false;
       installRustc = false;
     };
